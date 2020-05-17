b0a4bbb0366ba6c74a655d167247b2f70daf7d48
Ahmed Elhaddad <5328478+aihaddad@users.noreply.github.com>
2020-05-17 11:33:01 +0000

    Create S3 bucket and endpoint

--- main.tf
+++ main.tf
@@ -291,3 +291,43 @@ resource "aws_security_group" "wp_eds_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+# --- VPC Endpoint for S3 ---
+
+resource "aws_vpc_endpoint" "wp_private_s3_endpoint" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+  service_name = "com.amazonaws.${var.aws_region}.s3"
+  route_table_ids = [
+    "${aws_vpc.wp_vpc.default_route_table_id}",
+    "${aws_route_table.wp_public_rt.id}"
+  ]
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": "*",
+      "Resource": "*",
+      "Principal": "*"
+    }
+  ]
+}
+EOF
+}
+
+# --- S3 Bucket(s) ---
+
+resource "random_id" "wp_random" {
+  byte_length = 2
+}
+
+resource "aws_s3_bucket" "wp_code_bucket" {
+  bucket = "${var.domain_name}-${random_id.wp_random.dec}"
+  acl = "private"
+  force_destroy = true
+  
+  tags {
+    Name = "CodeBucket"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -2,10 +2,9 @@ variable "aws_region" {}
 variable "aws_profile" {}
 data "aws_availability_zones" "available" {}
 variable "vpc_cidr" {}
-
 variable "cidrs" {
   type = "map"
 }
-
 variable "localip" {}
 variable "cloudip" {}
+variable "domain_name" {}
