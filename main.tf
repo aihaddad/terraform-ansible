c665de853fbed6135b9c74824d8e8ecf0742781e
Ahmed Elhaddad <5328478+aihaddad@users.noreply.github.com>
2020-05-17 07:58:00 +0000

    Create VPC and subnets

--- main.tf
+++ main.tf
@@ -47,3 +47,144 @@ resource "aws_iam_role" "s3_access_role" {
 EOF
 }
 
+# --- VPC ---
+resource "aws_vpc" "wp_vpc" {
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags {
+    Name = "wp_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "wp_igw" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  tags {
+    Name = "wp_igw"
+  }
+}
+
+resource "aws_route_table" "wp_public_rt" {
+  vpc_id = "${aws_vpc.wp_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.wp_igw.id}"
+  }
+
+  tags {
+    Name = "wp_public"
+  }
+}
+
+resource "aws_default_route_table" "wp_private_rt" {
+  default_route_table_id = "${aws_vpc.wp_vpc.default_route_table_id}"
+
+  tags {
+    Name = "wp_private"
+  }
+}
+
+# The REPEAT EVERY SINGLE THING method :)
+resource "aws_subnet" "wp_public1" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public1"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_public1"
+  }
+}
+
+resource "aws_subnet" "wp_public2" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["public2"]}"
+  map_public_ip_on_launch = true
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_public2"
+  }
+}
+
+resource "aws_subnet" "wp_private1" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_private1"
+  }
+}
+
+resource "aws_subnet" "wp_private2" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["private2"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_private2"
+  }
+}
+
+resource "aws_subnet" "wp_rds1" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds1"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[0]}"
+
+  tags {
+    Name = "wp_rds2"
+  }
+}
+
+resource "aws_subnet" "wp_rds2" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds2"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[1]}"
+
+  tags {
+    Name = "wp_rds2"
+  }
+}
+
+resource "aws_subnet" "wp_rds3" {
+  vpc_id                  = "${aws_vpc.wp_vpc.id}"
+  cidr_block              = "${var.cidrs["rds3"]}"
+  map_public_ip_on_launch = false
+  availability_zone       = "${data.aws_availability_zones.available.names[2]}"
+
+  tags {
+    Name = "wp_rds3"
+  }
+}
+
+resource "aws_db_subnet_group" "wp_rds_subnet_group" {
+  name = "wp_rds_subnet_group"
+
+  subnet_ids = [
+    "${aws_subnet.wp_rds1.id}",
+    "${aws_subnet.wp_rds2.id}",
+    "${aws_subnet.wp_rds3.id}",
+  ]
+
+  tags {
+    Name = "wp_rds_sng"
+  }
+}
+
+resource "aws_route_table_association" "wp_public1_assoc" {
+  subnet_id      = "${aws_subnet.wp_public1.id}"
+  route_table_id = "${aws_route_table.wp_public_rt.id}"
+}
+
+resource "aws_route_table_association" "wp_public2_assoc" {
+  subnet_id      = "${aws_subnet.wp_public2.id}"
+  route_table_id = "${aws_route_table.wp_public_rt.id}"
+}
--- variables.tf
+++ variables.tf
@@ -1,2 +1,8 @@
 variable "aws_region" {}
 variable "aws_profile" {}
+data "aws_availability_zones" "available" {}
+variable "vpc_cidr" {}
+
+variable "cidrs" {
+  type = "map"
+}
