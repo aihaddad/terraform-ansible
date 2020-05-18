15719901953d6b9f5493264b494c2a9dfd8d3276
Ahmed Elhaddad <5328478+aihaddad@users.noreply.github.com>
2020-05-18 13:53:28 +0000

    Add autoscaling

--- variables.tf
+++ variables.tf
@@ -22,3 +22,9 @@ variable "elb_healthy_threshold" {}
 variable "elb_unhealthy_threshold" {}
 variable "elb_timeout" {}
 variable "elb_interval" {}
+variable "lc_instance_type" {}
+variable "asg_max" {}
+variable "asg_min" {}
+variable "asg_cap" {}
+variable "asg_grace" {}
+variable "asg_hct" {}
